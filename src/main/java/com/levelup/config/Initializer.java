8d652cfcb39481167066dbdf97a179400cdef5a9
Романа Краснова <romana.p51@gmail.com>
2016-05-11 20:02:32 +0300

    1

---
+++ src/main/java/com/levelup/config/Initializer.java
@@ -0,0 +1,40 @@
+package com.levelup.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+/**
+ * Class {@link com.levelup.config.Initializer} for data base configuration
+ *
+ * @author Skityashin Vladimir
+ * @version 1.0
+ * @since 11.05.16
+ */
+
+
+public class Initializer implements WebApplicationInitializer {
+
+    // Указываем имя нашему Servlet Dispatcher для мапинга
+    private static final String DISPATCHER_SERVLET_NAME = "dispatcher";
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+
+        // Регистрируем в контексте конфигурационный класс, который мы создадим ниже
+        ctx.register(WebAppConfig.class);
+        servletContext.addListener(new ContextLoaderListener(ctx));
+
+        ctx.setServletContext(servletContext);
+
+        ServletRegistration.Dynamic servlet = servletContext.addServlet(DISPATCHER_SERVLET_NAME,
+                new DispatcherServlet(ctx));
+        servlet.addMapping("/");
+        servlet.setLoadOnStartup(1);
+    }
+}
---
+++ src/main/java/com/levelup/config/WebAppConfig.java
@@ -0,0 +1,45 @@
+package com.levelup.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ * Class {@link com.levelup.config.WebAppConfig} for data base configuration
+ *
+ * @author Skityashin Vladimir
+ * @version 1.0
+ * @since 11.05.16
+ */
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.levelup")
+
+public class WebAppConfig extends WebMvcConfigurerAdapter {
+
+    // Позволяет видеть все ресурсы в папке pages, такие как картинки, стили и т.п.
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/WEB-INF/pages/**").addResourceLocations("/WEB-INF/pages/");
+    }
+
+    // а этот бин инициализирует View нашего проекта
+    // точно это же мы делали в web-dispatcher-servlet.xml
+    @Bean
+    public InternalResourceViewResolver setupViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/pages/");
+        resolver.setSuffix(".jsp");
+        resolver.setViewClass(JstlView.class);
+
+        return resolver;
+    }
+
+}
